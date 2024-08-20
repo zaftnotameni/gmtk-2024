ee20bfa45609469cfc1a6191d23206fc48c0d90a
TheNetherPug <91162221+TheNetherPug@users.noreply.github.com>
2024-08-20 10:39:51 -0400

    Fix

--- game/scripts/audio/player_menu_music.gd
+++ game/scripts/audio/player_menu_music.gd
@@ -7,3 +7,5 @@ func _ready() -> void:
 func _exit_tree() -> void:
 	AudioManager.bgm_title.stop()
 	AudioManager.play_bgm(AudioManager.bgm_levels_dynamic)
+	AudioManager.bgm_levels_dynamic.unmute(3)
+	AudioManager.bgm_levels_dynamic.percentage = 0
